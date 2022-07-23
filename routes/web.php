b5566807dc3b71d1a6ebbfce7112740ae1823734
Daniel Volski <danfvdaum3@hotmail.com>
2022-07-23 15:36:04 -0300

    Added routes index, about, contact and gallery

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/inicio', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/sobre', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contato', function () {
+    return view('mail');
+})->name('contact');
+
+Route::get('/gallery', function () {
+    return view('gallery');
+})->name('gallery');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
