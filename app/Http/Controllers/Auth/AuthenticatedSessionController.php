2f80adc703a23d3a70ea90379df1557f2ff05e8d
Daniel Volski <danfvdaum3@hotmail.com>
2022-08-19 15:52:04 -0300

    There is a dd() used for debug, REMOVE LATER

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
+        dd($request); //REMOVE!!!
         $request->authenticate();
 
         $request->session()->regenerate();
