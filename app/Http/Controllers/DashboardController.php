b8f55cd8b6e7c9455963343767793ccbd98460c9
LorenzoPrograming <lm.pilati@gmail.com>
2023-03-18 20:36:11 -0300

    Usuario autenticado

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     public function create(){
+        $user = auth()->user();
         $tasks = DB::table('users')
             ->join('tasks', 'users.id', '=', 'tasks.user_id')
+            ->where('user_id', $user->id)
             ->select('users.name as user_name', 'tasks.name as task_name', 'tasks.description')
             ->get();
         return view('dashboard')->with('tasks', $tasks);
