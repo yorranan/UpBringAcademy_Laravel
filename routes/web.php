573ebce80810e5ebb1c06390bc657cef46290c9e
Luiz Carlos <lsouzapedroso@gmail.com>
2023-03-26 21:17:39 -0300

    Fix web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ChangePasswordController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\GratificationController;
+use App\Http\Controllers\ChildController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SessionsController;
@@ -31,7 +32,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('dashboard', [DashboardController::class, 'create'])->name('dashboard');
 	Route::get('dashboardTask', [DashboardController::class, 'geTask'])->name('dashboardGetTask');
 	Route::get('profile', function () {return view('profile');})->name('profile');
-	Route::get('user-management', [InfoUserController::class, 'create'])->name('user-management');
+
 	Route::get('tables', function () {return view('tables');})->name('tables');
     Route::get('static-sign-in', function () {return view('static-sign-in');})->name('sign-in');
     Route::get('static-sign-up', function () {return view('static-sign-up');})->name('sign-up');
@@ -39,14 +40,14 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/user-profile', [InfoUserController::class, 'create']);
 	Route::post('/user-profile', [InfoUserController::class, 'store']);
     Route::get('/login', function () {return view('dashboard');})->name('sign-up');
-	
+
 	Route::get('/create-task', [TaskController::class, 'create'])->name('create-task');
 	Route::get('/add-task', [TaskController::class, 'add'])->name('add-task');
 	Route::get('/edit-task/{id}', [TaskController::class, 'edit'])->name('edit-task');
 	Route::post('/update-task/{id}', [TaskController::class, 'update'])->name('update-task');
 	Route::get('/delete-task/{id}', [TaskController::class, 'delete'])->name('delete-task');
 	Route::post('/task-store', [TaskController::class, 'store'])->name('task-store');
-	
+
 	Route::get('/create-gratification', [GratificationController::class, 'create'])->name('create-gratification');
 	Route::post('/gratification-store', [GratificationController::class, 'store'])->name('gratification-store');
     Route::get('/add-gratification', [GratificationController::class, 'add'])->name('add-gratification');
@@ -54,7 +55,13 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::post('/update-gratification/{id}', [GratificationController::class, 'update'])->name('update-gratification');
 	Route::get('/delete-gratification/{id}', [GratificationController::class, 'delete'])->name('delete-gratification');
 
-	Route::get('/new-user', [CreateTaskController::class, 'store'])->name('new-user'); //mudar
+    Route::get('user-management', [InfoUserController::class, 'create'])->name('user-management');
+	Route::get('/new-user', [ChildController::class, 'create'])->name('new-user');
+    Route::post('/user-store', [ChildController::class, 'store'])->name('user-store');
+
+
+
+
 });
 
 Route::group(['middleware' => 'guest'], function () {
